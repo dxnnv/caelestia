9afa22ce38b347882a52a589c58b1e21b0bedac5
danny <danny@atropos.cc>
2025-10-14 00:34:49 -0400

    refactor(cli): update args for clipboard

--- cli/src/caelestia/subcommands/clipboard.py
+++ cli/src/caelestia/subcommands/clipboard.py
@@ -7,6 +7,7 @@ from caelestia.command import BaseCommand, register
 def _configure(sub: ArgumentParser) -> None:
     sub.add_argument("--limit", type=int, default=20, help="Max items to show from cliphist")
     sub.add_argument("--copy", type=int, metavar="INDEX", help="Copy an item by index")
+    sub.add_argument("--delete", action="store_true", help="Delete entries from clipboard")
 
 
 @register("clipboard", help="Manage cliphist entries", configure_parser=_configure)
@@ -15,14 +16,11 @@ class Command(BaseCommand):
         clip = runner.check(["cliphist", "list"])
 
         if self.args.delete:
-            args = ["--prompt=del > ", "--placeholder=Delete from clipboard"]
-        else:
-            args = ["--placeholder=Type to search clipboard"]
-
-        chosen = runner.check(["fuzzel", "--dmenu", *args], input=clip)
-
-        if self.args.delete:
+            chosen = runner.check(
+                ["fuzzel", "--dmenu", "--prompt=del > ", "--placeholder=Delete from clipboard"], input=clip
+            )
             runner.run(["cliphist", "delete"], input=chosen)
         else:
+            chosen = runner.check(["fuzzel", "--dmenu", "--placeholder=Type to search clipboard"], input=clip)
             decoded = runner.check(["cliphist", "decode"], input=chosen)
             runner.run(["wl-copy"], input=decoded)
